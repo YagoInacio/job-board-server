8649abf94b762cb47a7ea34b79bec2750353e6c3
Yago Inacio <yagofaran@gmail.com>
2023-11-18 14:00:05 -0300

    fixed response on create candidate endpoint

--- src/main/java/dev/yagofaran/jobboardserver/modules/candidate/controllers/CandidateController.java
+++ src/main/java/dev/yagofaran/jobboardserver/modules/candidate/controllers/CandidateController.java
@@ -4,6 +4,7 @@ import dev.yagofaran.jobboardserver.modules.candidate.entities.CandidateEntity;
 import dev.yagofaran.jobboardserver.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,9 @@ public class CandidateController {
     private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        return this.createCandidateUseCase.execute(candidateEntity);
+    public ResponseEntity<CandidateEntity> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        var result =  this.createCandidateUseCase.execute(candidateEntity);
+
+        return ResponseEntity.status(201).body(result);
     }
 }
