ebf570d8fcea8a20eaab6379e7d729cb2b078818
Yago Inacio <yagofaran@gmail.com>
2023-11-18 10:14:47 -0300

    Added Lombok dependency, created base code for first endpoint

---
+++ src/main/java/dev/yagofaran/jobboardserver/modules/candidate/entities/CandidateEntity.java
@@ -0,0 +1,16 @@
+package dev.yagofaran.jobboardserver.modules.candidate.entities;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+}
