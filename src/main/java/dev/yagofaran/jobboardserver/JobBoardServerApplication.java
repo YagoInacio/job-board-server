a6d9d8994ff880888c11655d5db971d3c2d89937
Yago Inacio <yagofaran@gmail.com>
2023-11-18 09:45:52 -0300

    first commit

---
+++ src/main/java/dev/yagofaran/jobboardserver/JobBoardServerApplication.java
@@ -0,0 +1,13 @@
+package dev.yagofaran.jobboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yagofaran/jobboardserver/JobBoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yagofaran.jobboardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
