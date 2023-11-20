b49c142b81bb21665f5a482f1c215497bc33ad69
Yago Inacio <yagofaran@gmail.com>
2023-11-20 10:55:56 -0300

    encoding password before storing on DB and guarded the create job route

--- src/main/java/dev/yagofaran/jobboardserver/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/dev/yagofaran/jobboardserver/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,12 +4,15 @@ import dev.yagofaran.jobboardserver.exceptions.AppException;
 import dev.yagofaran.jobboardserver.modules.company.entities.CompanyEntity;
 import dev.yagofaran.jobboardserver.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public CompanyEntity execute(CompanyEntity companyEntity) {
         this.companyRepository
@@ -17,6 +20,9 @@ public class CreateCompanyUseCase {
             .ifPresent((company) -> {
                 throw new AppException("User already exists");
             });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
     }
