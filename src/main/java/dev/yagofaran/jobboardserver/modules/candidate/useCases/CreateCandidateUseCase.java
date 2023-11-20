6ab759b0fc03dd09cb0de47ac4f3028c9251743e
Yago Inacio <yagofaran@gmail.com>
2023-11-20 11:56:33 -0300

    encrypting candidate password before saving on DB

--- src/main/java/dev/yagofaran/jobboardserver/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/dev/yagofaran/jobboardserver/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,12 +4,15 @@ import dev.yagofaran.jobboardserver.exceptions.AppException;
 import dev.yagofaran.jobboardserver.modules.candidate.entities.CandidateEntity;
 import dev.yagofaran.jobboardserver.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
@@ -17,6 +20,9 @@ public class CreateCandidateUseCase {
                 .ifPresent((user) -> {
                     throw new AppException("User already exists");
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
