ff20328c8905bc214dffb2219362df1425498358
Yago Inacio <yagofaran@gmail.com>
2023-11-21 17:19:15 -0300

    created independent class for swagger configuration

--- src/main/java/dev/yagofaran/jobboardserver/JobBoardServerApplication.java
+++ src/main/java/dev/yagofaran/jobboardserver/JobBoardServerApplication.java
@@ -1,30 +1,11 @@
 package dev.yagofaran.jobboardserver;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Locale;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-	info = @Info(
-		title = "Job Board",
-		description = "Job board manager API",
-		version = "1"
-	)
-)
-@SecurityScheme(
-	name = "jwt_auth",
-	scheme = "bearer",
-	bearerFormat = "JWT",
-	type = SecuritySchemeType.HTTP,
-	in = SecuritySchemeIn.HEADER
-)
 public class JobBoardServerApplication {
 
 	public static void main(String[] args) {
